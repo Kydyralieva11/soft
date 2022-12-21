37819b3b666830da19c0dd3b90a23659aa741e88
Kydyralieva11 <117450496+Kydyralieva11@users.noreply.github.com>
2022-12-21 14:21:18 +0600

    solution problems

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,10 +20,10 @@ var swiper = new Swiper('.mySwiper', {
       el: '.swiper-pagination',
       type: 'fraction',
     },
-    // autoplay: {
-    //     delay: 2500,
-    //     disableOnInteraction: false,
-    // },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
